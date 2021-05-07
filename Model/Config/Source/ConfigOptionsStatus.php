82d4b87643abddc8e4d362ea9d3111a74be0a183
Yaroslav Kozar <slavik.kozar3@gmail.com>
2021-05-07 14:05:20 +0300

    Module for Canceling orders from customer account

---
+++ Model/Config/Source/ConfigOptionsStatus.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Kozar\AddCancelOrder\Model\Config\Source;
+
+class ConfigOptionsStatus implements \Magento\Framework\Option\ArrayInterface
+{
+    public function toOptionArray()
+    {
+        return [
+            ['value' => 'Pending', 'label' => __('Pending')],
+            ['value' => 'Processing', 'label' => __('Processing')],
+            ['value' => 'Suspected Fraud', 'label' => __('Suspected Fraud')],
+        ];
+    }
+}
